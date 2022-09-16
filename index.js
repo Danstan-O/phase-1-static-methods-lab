74b1938d10dc78632bfd3cf12d1a4752836ad574
danstan <danstan.oduor@student.moringaschool.com>
2022-09-16 17:53:06 +0300

    stattic

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+
+
+
+static capitalize(string){
+  return string.charAt(0).toUpperCase() + string.slice(1);
+  
+  
+
 }
+static sanitize(alpha){
+  return alpha.replace(/[^a-z0-9]\s-/gi, '' );
+
+}
+static titleize(words){
+  return words.split( '')[0].toUpperCase( ).join( '');
+}
+
+}
+
+const obj = new Formatter();
+obj.speak(); 
+const speak = obj.speak;
+speak(); 
+
+Formatter.capitalize() 
+const capitalize = Formatter.capitalize;
+capitalize();
